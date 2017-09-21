44739fb2b1d691568e0abcddf9dd101c644c114d
Alex Casajuana <alexcm.14@gmail.com>
2017-09-21 12:24:06 +0200

    test: type checker happy path test cases

--- test/TypeChecker.test.js
+++ test/TypeChecker.test.js
@@ -5,32 +5,66 @@ import TypeChecker from '../src/TypeChecker';
  * Query object tests
  */
 describe('# Test: TypeChecker()', () => {
-    describe('-> When checking types', () => {
+    describe('-> isDefined()', () => {
         it('should throw exception when value is not defined', () => {
             let undefinedValue;
             expect(() => TypeChecker.isDefined(undefinedValue)).to.throw(TypeError);
         });
 
+        it('should silent pass if value is defined', () => {
+            let definedValue = 1;
+            expect(() => TypeChecker.isDefined(definedValue)).to.not.throw(TypeError);
+        });
+    });
+
+    describe('-> isBool()', () => {
         it('should throw exception when value is not type Boolean', () => {
             let notABooleanValue = 'I am a string';
             expect(() => TypeChecker.isBool(notABooleanValue)).to.throw(TypeError);
         });
 
+        it('should silent pass if value is type of Boolean', () => {
+            let booleanValue = true;
+            expect(() => TypeChecker.isBool(booleanValue)).to.not.throw(TypeError);
+        });
+    });
+
+    describe('-> isString()', () => {
         it('should throw exception when value is not type String', () => {
             let notAStringValue = true;
             expect(() => TypeChecker.isString(notAStringValue)).to.throw(TypeError);
         });
 
+        it('should silent pass if value is type of String', () => {
+            let stringValue = 'I am a string';
+            expect(() => TypeChecker.isString(stringValue)).to.not.throw(TypeError);
+        });
+    });
+
+    describe('-> isArray()', () => {
         it('should throw exception when value is not type Array', () => {
             let notAnArrayValue = {};
             expect(() => TypeChecker.isArray(notAnArrayValue)).to.throw(TypeError);
         });
 
-        it('should throw exception when the given object is not the expected type', () => {
-            class A {}
-            class B {}
+        it('should silent pass if value is type of Array', () => {
+            let arrayValue = ['I am a string', 'inside of an array'];
+            expect(() => TypeChecker.isArray(arrayValue)).to.not.throw(TypeError);
+        });
+    });
+
+    describe('-> isObjectTypeOf()', () => {
+        class A {}
+        class B {}
+
+        it('should throw exception when the given object is not equal to the expected type', () => {
             const classB = new B;
             expect(() => TypeChecker.isObjectTypeOf(classB, A)).to.throw(TypeError);
+        });
+
+        it('should silent pass if given object type is equal to the expected type', () => {
+            const classA = new A;
+            expect(() => TypeChecker.isObjectTypeOf(classA, A)).to.not.throw(TypeError);
         });
     });
 });
